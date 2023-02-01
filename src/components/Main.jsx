912129fd8329e8a9b643de45f0ba6bd6c29dc667
Katie Brown <katieleebrown19@gmail.com>
2023-02-01 16:23:31 -0500

    props and api calls

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,39 @@
 import { useState } from "react";
 
 function Main(props) {
-    return (
-        <div>
+    const [photo, setPhoto] = useState({
+        photo: "",
+        name: "",
+        date: "",
+        description: "",
+        copyright: "",
+        mediaType: ""
+    })
 
-        </div>
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=veITwoMdS2sBK7aaxe0IJGKKAqqKjgaeLRcLQDtw`)
+        .then(res => res.json())
+        .then(data => setPhoto(prevPhoto => {
+            return {
+                photo: data.url,
+                name: data.title,
+                date: data.date,
+                description: data.explanation,
+                copyright: data.copyright,
+                mediaType: data.media_type
+            }
+        }))
+
+    return (
+        <main className={props.darkMode ? "dark" : ""}>
+            <div className="photo--card">
+                <h1>NASA's Astronomy Photo of the Day</h1>
+                <p>{photo.date}</p>
+                <img src={photo.photo} alt={photo.name} />
+                <h3>{photo.name}</h3>
+                <small>Photo Credit: {photo.copyright}</small>
+                <p>{photo.description}</p>
+            </div>
+        </main>
     )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,17 @@ import { useState } from "react";
 
 function Navbar(props) {
     return (
-        <nav>
+        <nav className={props.darkMode ? "dark" : ""}>
+            <img className="nav--logo_icon" src="" alt="" />
+            <h3>NASA Daily</h3>
 
+            <div className="toggler">
+                <p className="toggler--light">Light</p>
+                <div className="toggler--slider" onClick={props.toggleMode}>
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
         </nav>
     )
 }
